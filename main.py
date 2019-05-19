1bd8b65108dd85e6e6712dc5b682e4261e250af0
JaeHyeon-Yu <43131666+JaeHyeon-Yu@users.noreply.github.com>
2019-05-19 16:50:06 +0900

    프로젝트 및 메인파일 생성
    프로젝트와 메인 파일을 생성하고
    메뉴를 선택해서 무한루프 돌게함
    
    메뉴 출력 및 선택은 구현안함

---
+++ main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+active_loop = True
+def PrintMenu():
+    pass
+def launcherFunction(menu):
+    pass
+while active_loop is True:
+    PrintMenu()
+    menuKey = str(input('select menu :'))
+    launcherFunction(menuKey)
+
+# http://openAPI.seoul.go.kr:8088/(인증키)/xml/ListCulturalAssetsInfo/1/5/(일련번호)
