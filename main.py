65a974a80522eddc31bb1edb27a9d5d94c24f8f4
JaeHyeon-Yu <43131666+JaeHyeon-Yu@users.noreply.github.com>
2019-05-22 22:47:21 +0900

    이름 출력 구현
    xml을 읽어서 범위 내의 문화재의 한글명을 출력할 수 있다.

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 # -*- coding: utf-8 -*-
-import openapi
+from openapi import *
 active_loop = True
 def PrintMenu():
     print("search : s")
 def launcherFunction(menu):
     if menu is 's':
         key = str(input('input key : '))
-        openapi.LoadXML2API(key)
+        LoadXML2API(key)
+        # print(a)
+
+        #print(type(a))
+        #PrintItem(a)
+
+
+        # aa = a.getElementsByTagName("NAME_KOR")
+
+        # print(a.getElementsByTagName("NAME_KOR"))
+def PrintItem(item):
+    # print(type(item))
+    print(item.find("NAME_KOR"))
+    print(item)
+    # print ("Name : ", item.attrib["NAME_KOR"])
+    # for i in item.iter("")
+
+    pass
 while active_loop is True:
     PrintMenu()
     menuKey = str(input('select menu :'))
     launcherFunction(menuKey)
-
-# http://openAPI.seoul.go.kr:8088/(인증키)/xml/ListCulturalAssetsInfo/1/5/(일련번호)
--- openapi.py
+++ openapi.py
@@ -1,11 +1,39 @@
 # -*- coding:cp949 -*-
 import os
 import sys
-import urllib.request
+import xml
+import urllib
+from urllib.request import urlopen
 from xml.dom.minidom import parseString
+# from xml.etree import ElementTree
 password = '484350454a7a6f733431776467704b' # 서울데이터포털 인증키
 
 def LoadXML2API(key):
+    import http.client
+    url = "/" + password + "/xml/ListCulturalAssetsInfo/1/5/"
+    conn = http.client.HTTPConnection("openAPI.seoul.go.kr:8088")
+    conn.request("GET", url)
+    req = conn.getresponse()
+
+    cultureDoc = req.read().decode('utf-8')
+
+    if cultureDoc is None:
+        print("Error!")
+    else:
+        parseData = parseString(cultureDoc)
+        InfoCulture = parseData.childNodes
+        row = InfoCulture[0].childNodes
+        dataLst = []
+        for item in row:
+            if item.nodeName == "row":
+                subitems = item.childNodes
+                # tel = str(subitems[1].firstChild.nodeValue)
+                dataLst.append((subitems[3].childNodes))
+
+        for i in range(len(dataLst)):
+            print(dataLst[i][0])
+
+    pass
     url = "http://openAPI.seoul.go.kr:8088/" + password + "/xml/ListCulturalAssetsInfo/1/5/" + key
     resp = None
 
@@ -20,5 +48,24 @@ def LoadXML2API(key):
         print("error code=" + e.code)
         print(parseString(e.read().decode('utf-8')).toprettyxml())
     else:
-        response_body = resp.read()
-        print(parseString(response_body.decode('utf-8')).toprettyxml())
+        response_body = resp.read().decode('utf-8')
+        # print(parseString(response_body.decode('utf-8')).toprettyxml())
+        body = xml.dom.minidom
+        body = parseString(response_body).toprettyxml()
+
+
+
+        # doc = parseString(response_body)
+
+        # print(type(doc))
+        # doc = parse(response_body)
+        # doc = ET.parse(response_body)
+        # doc = ET.parse(doc)
+        return body
+
+def LoadXML2API2(key):
+    url = "http://openAPI.seoul.go.kr:8088/" + password + "/xml/ListCulturalAssetsInfo/1/5/" + key
+    u = urlopen(url)
+    doc = ElementTree.parse(u)
+
+    return doc
