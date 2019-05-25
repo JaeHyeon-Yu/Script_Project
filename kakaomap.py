9f0d5dd857a321d62d3966c60202b0ec14cc7122
JaeHyeon-Yu <43131666+JaeHyeon-Yu@users.noreply.github.com>
2019-05-25 23:07:25 +0900

    Kakao API 한글검색 구현
    한글 검색시 나오던 유니코드 에러 수정

--- kakaomap.py
+++ kakaomap.py
@@ -1,14 +1,15 @@
 # -*- coding:utf-8 -*-
 import http.client
-
+import urllib
 # password = "42db2ecfe7ba9890e3152d176b35c4d4"
 password = "42db2ecfe7ba9890e3152d176b35c4d4"
 
 def SearchPos():
     header = {'Authorization': 'KakaoAK ' + password}
     conn = http.client.HTTPSConnection("dapi.kakao.com")
-    conn.request("GET", "/v2/local/geo/coord2regioncode.xml?x=127.1086228&y=37.4012191", headers=header)
-
+    # conn.request("GET", "/v2/local/geo/coord2regioncode.xml?x=127.1086228&y=37.4012191", headers=header)
+    keyVal = urllib.parse.quote("봉황각")
+    conn.request("GET", "/v2/local/search/keyword.xml?y=37.514322572335935&x=127.06283102249932&radius=20000&query="+keyVal, headers=header)
     req = conn.getresponse()
     print(req.status, req.reason)
 
