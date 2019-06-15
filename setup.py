505f7c81fed65958cc1870d636c13034fa0ea50b
JaeHyeon-Yu <43131666+JaeHyeon-Yu@users.noreply.github.com>
2019-06-15 18:53:02 +0900

    setup설정
    내 모듈들을 사용하여 설치파일을 만들도록 setup파일 설정

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from distutils.core import setup
 
 setup(name='book',
       version='1.0',
-      py_modules=['book']
+      py_modules=['Defined_Var', 'gmail', 'kakaomap', 'main', 'mainframe', 'Map', 'sysmtplib', 'noti', 'openapi', 'teller']
       )
